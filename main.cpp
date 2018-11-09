14e1a848ddee78bf4f75557774a2dce8538aa6f9
Abe Ramseyer <abe.ramseyer@gmail.com>
2018-11-08 22:57:13 -0600

    usable code

--- main.cpp
+++ main.cpp
@@ -4,19 +4,17 @@
 #include <stdlib.h>
 #include <vector>
 #include <set>
+#include <math.h>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/adjacency_iterator.hpp>
-#include <boost/graph/graphviz.hpp>
 #include <boost/range/algorithm_ext/push_back.hpp>
 #include <boost/range/irange.hpp>
 
 using namespace boost;
 
 typedef adjacency_list<setS, vecS, undirectedS> Graph;
-//typedef graph_traits<Graph>::edge_iterator edge_iterator;
 
 std::vector<std::vector<int>> maximal_cliques;
-
 Graph g;
 
 std::vector<int> intersection (std::vector<int> v1, std::vector<int> v2) {
@@ -48,26 +46,6 @@ void bron_kerbosch (std::vector<int> r, std::vector<int> p, std::vector<int> x, 
 	}
 }
 
-std::vector<std::vector<std::vector<int>>> combinations(std::vector<std::vector<int>> els) {
-	unsigned long size = els.size();
-	if (size == 0) {
-		std::vector<std::vector<std::vector<int>>> ret;
-		ret.emplace_back();
-		return ret;
-	}
-	std::vector<int> p = els[size - 1];
-	els.pop_back();
-	std::vector<std::vector<std::vector<int>>> subCombs = combinations(els);
-	els.push_back(p);
-	std::vector<std::vector<std::vector<int>>> copy = std::vector<std::vector<std::vector<int>>>(subCombs);
-	for (auto &subComb : subCombs) {
-        subComb.push_back(p);
-	}
-	std::vector<std::vector<std::vector<int>>> un;
-	std::set_union(copy.begin(), copy.end(), subCombs.begin(), subCombs.end(), std::inserter(un, un.end()));
-	return un;
-}
-
 int main(int argc, char* argv[]) {
 	std::string current_exec_name = argv[0];
 	std::string input_filename;
@@ -78,6 +56,10 @@ int main(int argc, char* argv[]) {
 	}
 	input_filename = argv[1];
 	std::ifstream input_file(input_filename);
+	if (!input_file.good()) {
+		std::cout << "File " << input_filename << " couldn't be opened for reading" << std::endl;
+		return 0;
+	}
 	int name1, name2;
 	// format of file always has 2 nodes on every line
 	while(input_file >> name1 >> name2) {
@@ -88,52 +70,51 @@ int main(int argc, char* argv[]) {
 	push_back(all_vertices, irange(0, (int)num_vertices(g)));
 	std::cout << "vertex count: " << (int)num_vertices(g) << ", finding maximal cliques.." << std::endl;
 	bron_kerbosch(std::vector<int>(), all_vertices, std::vector<int>(), 0);
-	std::cout << "We found " << maximal_cliques.size() << " maximal cliques.\n";
+	std::cout << "We found " << maximal_cliques.size() << " maximal cliques." << std::endl;
 //	for (const std::vector<int> c: maximal_cliques) {
 //		std::cout << "Clique: ";
 //		for (const int v: c) std::cout << v << " ";
 //		std::cout << std::endl;
 //	}
 
-	std::vector<std::vector<std::vector<int>>> combos = combinations(maximal_cliques);
-	combos.erase(combos.begin());
-//	for(auto a : combos) {
-//		std::cout << "set: " << std::endl;
-//		for (auto b : a) {
-//			for (auto c : b) {
-//				std::cout << c;
-//			}
-//			std::cout << std::endl;
-//		}
-//		std::cout << std::endl;
-//	}
-	std::vector<std::vector<int>> *result = nullptr;
-	std::cout << "Number of combinations to check: " << combos.size() << std::endl;
-	for (std::vector<std::vector<int>> &f : combos) {
-		// create a set of all the vertices covered by this set of maximal clique cover
-		std::set<int> combination_verticies;
-		for(std::vector<int> g : f) { // each graph in a single combination
-			for (int h : g) { // each vertex in a single graph
-				combination_verticies.insert(h);
+	unsigned long size = maximal_cliques.size();
+
+	auto num_combos = (long long) pow(2, size);
+	std::vector<std::vector<int>*> result;
+	for (int i = 1; i < num_combos; i++) {
+		// the set of cliques that is possibly a (minimal) clique cover
+		std::vector<std::vector<int>*> candidate;
+		// all the vertices that the candidate has within it
+		std::set<int> ints_in_clique_set;
+		for (int j = 0; j < size; j++) {
+			// creates every possible combination of the maximal cliques
+			if ((int)(i / pow(2, j)) % 2 == 0) {
+				for(const int a : maximal_cliques[j]) {
+					ints_in_clique_set.insert(a);
+				}
+				candidate.push_back(&maximal_cliques[j]);
 			}
 		}
-		// check clique cover by computing the difference of verticies between those contained in this set of graphs and
-		// all verticies in the entire graph (should be 0 if all vertices are in both)
-		std::vector<int> diff;
-		std::set_difference(all_vertices.begin(), all_vertices.end(), combination_verticies.begin(), combination_verticies.end(), std::inserter(diff, diff.begin()));
-		// determine if this is a clique cover
-		if (!diff.empty())
-			continue;
-		// && the size of this candidates element < fewest
-		if (result == nullptr || f.size() < result->size()) {
-			result = &f;
-		}
+		// save some time by only printing progress every thousand or so iterations
+		if (i % 2048 == 0) {
+			std::cout << "Percent Complete: " << ((float) i / num_combos) * 100 << "%            \r"; // these spaces are necessary
+        }
+        // determine if this is a clique cover
+        if (ints_in_clique_set.size() != all_vertices.size()) {
+            continue;
+        }
+        // && the size of this candidates element < fewest
+        if (result.empty() || candidate.size() < result.size()) {
+            std::cout << "found new minimal clique cover, size = " << candidate.size() << std::endl;
+            result = candidate;
+        }
 	}
-	if (result != nullptr) {
-		std::cout << "yay found one of size " << result->size() << std::endl;
+	std::cout << std::endl;
+	if (!result.empty()) {
+		std::cout << "yay found one of size " << result.size() << std::endl;
 		std::cout << "Minimum Clique Cover: " << std::endl;
-		for (const std::vector<int>& clique : *result) {
-			for (const int &v: clique)
+		for (const std::vector<int>* clique : result) {
+			for (const int &v: *clique)
 				std::cout << v << " ";
 			std::cout << std::endl;
 		}
@@ -143,6 +124,5 @@ int main(int argc, char* argv[]) {
 	}
 
     std::cout << std::endl << "Your clique cover is huge!" << std::endl;
-
     return 0;
 }
